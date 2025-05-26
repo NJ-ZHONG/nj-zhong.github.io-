d62bd514cf0948a7f9cbc7de92332b3be088a503
NJ-ZHONG <make5855012922@gmail.com>
2025-05-26 17:44:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navLinks = document.querySelector('.nav-links');
+
+  if (hamburgerMenu && navLinks) {
+    hamburgerMenu.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+  }
+});
